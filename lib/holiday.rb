0e4312bc8dc407121c35a8113d168473ccbf7e75
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-21 23:47:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -101,7 +101,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |attribute, value|
-    puts "#{attribute}:"
+    #puts "#{attribute}:"
     value.each do |seasons|
       binding.pry
     end
