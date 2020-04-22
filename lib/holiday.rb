4c440df691b81e3d3aaf28158e3fe036db1adcfa
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-22 01:14:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -114,7 +114,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |seasons, season_hash|
     season_hash.each do |holidays, supplies|
-      supplies.each{|i| puts "#{holidays.split}" if i == "BBQ" }
+      supplies.each{|i| puts "#{holidays.split("\n")}" if i == "BBQ" }
     end
   end
 end
